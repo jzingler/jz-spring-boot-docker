c9a19d0bff88acf0634168e7d49f04c6d13680a4
jzingler <jzingler@pivotal.io>
2019-03-05 15:12:41 -0800

    pre-amazon linux working

--- complete/start.sh
+++ complete/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Start the second process
-java -Djavax.net.debug=all \
-    -Djava.ext.dirs=cloud-security-provider/container-security-provider-1.16.0_RELEASE.jar:${JAVA_HOME}/lib/ext \
-    -Djava.security.properties=cloud-security-provider/java.security \
-    -jar app.jar
+# java -Djava.ext.dirs=/cloud-security-provider/container-security-provider-1.16.0_RELEASE.jar:${JAVA_HOME}/lib/ext \
+#     -Djava.security.properties=/cloud-security-provider/java.security \
+#     -jar app.jar
+
+exec java -Djava.ext.dirs=/cloud-security-provider:${JAVA_HOME}/lib/ext \
+    -Djava.security.properties=/cloud-security-provider/java.security \
+    -cp app:app/lib/* hello.Application    
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start java process: $status"
